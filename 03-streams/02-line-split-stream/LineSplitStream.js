011024d02c4b83acb9bfd04ff753286ea5dd1a44
Artsiom Sarychau <sarychevas4@gmail.com>
2020-12-15 06:35:45 +0300

    3rd task-02

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -7,9 +7,21 @@ class LineSplitStream extends stream.Transform {
   }
 
   _transform(chunk, encoding, callback) {
+    
+    const lines = ((this.temporal != null ? this.temporal: "") + chunk.toString()).split(/\r?\n/);
+    
+    this.temporal = lines.pop();
+        
+    for (let line of lines) {
+       this.push(line); }
+    
+    callback();
   }
 
   _flush(callback) {
+    this.push(this.temporal != null ? this.temporal:"");
+   
+    callback();
   }
 }
 
